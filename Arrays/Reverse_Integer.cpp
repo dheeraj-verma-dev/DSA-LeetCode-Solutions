8d7249c7e5b9fed0ad0c2c6109561ee4c847c544
dheeraj-verma-dev <dheerajverma.dev@gmail.com>
2026-04-29 10:08:55 +0530

    Day 33 : Re solved the reversed Problem

--- Arrays/Reverse_Integer.cpp
+++ Arrays/Reverse_Integer.cpp
@@ -14,13 +14,26 @@
 class Solution {
 public:
     int reverse(int x) {
-    long long rev = 0;
-    while(x != 0){
-        int ld = x % 10;
-        rev = rev * 10 + ld;
-        x/=10;
-    }
-    if(rev > INT_MAX || rev < INT_MIN) return 0;
-    return (int ) rev;
+        long long rev = 0;
+        bool isNegative = false;
+        long long num = x;
+        if(num < 0){
+            isNegative = true;
+            num = -num;
+        }
+        
+        while(num > 0){
+            int lastDigit = num % 10;
+            rev = rev * 10 + lastDigit;
+            num /= 10;
+        }
+
+        if(isNegative == true) {
+            rev = -rev;
+        }
+
+        if(rev > INT_MAX || rev < INT_MIN) return 0;
+
+        return rev;
     }
 };
