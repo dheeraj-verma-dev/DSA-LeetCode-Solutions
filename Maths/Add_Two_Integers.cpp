4abf48aa38e1d5f548144790511caeb237c859f8
dheeraj-verma-dev <dheerajverma.dev@gmail.com>
2026-05-01 10:32:51 +0530

    Day 35 : Add Two Integers

---
+++ Maths/Add_Two_Integers.cpp
@@ -0,0 +1,15 @@
+// Problem: Add Two Integers
+// Platform: LeetCode
+// Approach: Direct Arithmetic
+// Time Complexity: O(1)
+// Space Complexity: O(1)
+
+// Explanation:
+// Return the sum of the two given integers.
+
+class Solution {
+public:
+    int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+};
